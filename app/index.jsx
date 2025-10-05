45eec4389391addc0e2a35c4c469c3b962f9886b
CamoPaw252 <93169276+CamoPaw252@users.noreply.github.com>
2025-10-05 16:51:00 -0600

    Scrap Project, starting fresh

--- app/index.jsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from 'expo-router';
-
-// This is the main entry point for the app
-// We redirect to the auth flow by default
-export default function Index() {
-  return <Redirect href="/(auth)/login" />;
-}
