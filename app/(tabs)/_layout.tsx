6d5c531404740b22258a38e1f392fb682cc7ecd7
CamoPaw252 <93169276+CamoPaw252@users.noreply.github.com>
2025-10-11 17:25:23 -0600

    New Exercise Button

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Tabs } from "expo-router";
+import { StyleSheet } from 'react-native';
 
 function TabBarIcon({ name, color }: { name: string; color: string }) {
   return <FontAwesome6 name={name} size={24} style={{ marginBottom: -3 }} color={color} />;
@@ -12,21 +13,28 @@ export default function TabsLayout() {
       screenOptions={{
         headerShown: true,
       }}
-    >
+      >
       <Tabs.Screen
         name="exercises"
         options={{
           title: "Exercises",
           tabBarIcon: ({ color }) => <TabBarIcon name="dumbbell" color={color} />,
         }}
-      />
+        />
       <Tabs.Screen
         name="workout"
         options={{
           title: "Workout",
           tabBarIcon: ({ color }) => <TabBarIcon name="plus" color={color} />,
         }}
-      />
+        />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a44848ff', 
+  },
+});
--- app/(tabs)/exercises.tsx
+++ app/(tabs)/exercises.tsx
@@ -1,11 +1,48 @@
 import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ExerciseSearchList } from '../../components/ExerciseSearchList';
 
 export default function ExercisesScreen() {
   const handleSelect = (exercise: { name: string; category: string; muscles: string[] }) => {
-    console.log('Selected exercise:', exercise.name);
-    // TODO: Navigate to detail screen or add to workout
+    console.log('Selected Exercise:', exercise.name);
   };
 
-  return <ExerciseSearchList onSelect={handleSelect} />;
+  const handleNewExercise = () => {
+    console.log("New Exercise pressed");
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff"}}>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.newButton} onPress={handleNewExercise}>
+          <Text style={styles.buttonText}>New</Text>
+        </TouchableOpacity>
+      </View>
+
+      <ExerciseSearchList onSelect={handleSelect} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 16, backgroundColor: '#fff' },
+  header: { fontSize: 24, fontWeight: 'bold', marginBottom: 12 },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginTop: 10,
+    marginRight: 10,
+  },
+  newButton: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '500',
+  },
+});
+
